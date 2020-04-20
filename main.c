fe20217e5458ed2412022b4e483992cd6c94fdfe
Sami-Sh99 <sami_sh.99@outlook.com>
2020-04-20 18:54:36 +0300

    Integrated factorial file to main file

--- main.c
+++ main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
-
+#include "factorial"
 void int main(int argc, char const *argv[])
 {
-	printf("Hello Github !!");
+	printf("Hello Github !! \n");
+
+	printf("%d\n",factorial(5));
 	return 0;
 }
