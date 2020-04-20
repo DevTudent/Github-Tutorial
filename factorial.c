8f484ed8bf01e3ed0c10c8482bab9ca967e79e84
DevTudent <44131118+DevTudent@users.noreply.github.com>
2020-04-20 19:30:53 +0300

    Updated Factorial Function
    Iterative faster that recursive

--- factorial.c
+++ factorial.c
@@ -2,10 +2,11 @@
 
 int find_factorial(int n)
 {
-   //Factorial of 0 is 1 
-   if(n==0)
-      return(1);
- 
-   //Function calling itself: recursion
-   return(n*find_factorial(n-1));
+	int fact = 1;
+	int i;
+
+	for (i = 1; i <= n; i++)
+		fact = fact * i;
+
+	return fact;
 }
